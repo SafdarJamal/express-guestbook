acd39d250eae43075427b1cd23129aa0679d797d
Safdar Jamal <safdar.dev@gmail.com>
2020-05-30 22:02:40 +0500

    Create hello world server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const HOST = '0.0.0.0';
+const PORT = 8080;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
