bc8919a979845a275dc773a320508722d7c17432
Safdar Jamal <safdar.dev@gmail.com>
2020-05-31 21:49:50 +0500

    MVP complete

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const newEntryRouter = require('./routes/new-entry');
+
 const app = express();
 
-const HOST = '0.0.0.0';
-const PORT = 8080;
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+const entries = [];
+app.locals.entries = entries;
+
+app.use('/', indexRouter);
+app.use('/new-entry', newEntryRouter);
+
+app.use((req, res) => res.status(404).render('404'));
+
+module.exports = app;
