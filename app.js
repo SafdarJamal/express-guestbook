0d151b515428f2497c7865fb7f272e6a06f534e0
Safdar Jamal <safdar.dev@gmail.com>
2020-06-01 21:21:45 +0500

    Integrate helmet and create database connection

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const helmet = require('helmet');
 const logger = require('morgan');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const newEntryRouter = require('./routes/new-entry');
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+mongoose
+  .connect(process.env.MONGO_DB_URI, options)
+  .then(() => console.log('Connected to DB!'))
+  .catch(error => console.log(error));
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-const entries = [];
-app.locals.entries = entries;
 
 app.use('/', indexRouter);
 app.use('/new-entry', newEntryRouter);
