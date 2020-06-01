4c323ec3a6a91b175bdfed778707957c4dc2a9ad
Safdar Jamal <safdar.dev@gmail.com>
2020-06-01 23:00:18 +0500

    Refactor routes and views

--- routes/entries/index.js
+++ routes/entries/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const entriesRouter = require('./entries');
 const newRouter = require('./new');
 
-router.get('/', (req, res) => res.render('entries'));
+router.use('/', entriesRouter);
 router.use('/new', newRouter);
 
 module.exports = router;
--- routes/entries/new.js
+++ routes/entries/new.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.render('new'));
+router.get('/', (req, res) => res.render('entries/new'));
 
 router.post('/', (req, res) => {
   res.redirect('/entries');
