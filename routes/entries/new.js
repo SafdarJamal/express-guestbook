c9d4be940231f9c5b29dc12b9f3ad1e52486e411
Safdar Jamal <safdar.dev@gmail.com>
2020-06-02 22:08:14 +0500

    Create entry's post endpoint

--- routes/entries/new.js
+++ routes/entries/new.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const Entry = require('../../models/Entry');
+
 router.get('/', (req, res) => res.render('entries/new'));
 
 router.post('/', (req, res) => {
-  res.redirect('/entries');
+  const { name, email, comment } = req.body;
+
+  const entry = new Entry({ name, email, comment });
+
+  entry
+    .save()
+    .then(data => res.status(200).redirect('/entries'))
+    .catch(error => res.status(400).send({ message: error.message }));
 });
 
 module.exports = router;
