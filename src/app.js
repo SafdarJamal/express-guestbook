bb06ea77db7476e5c739ef368cffc2078446432e
Safdar Jamal <safdar.dev@gmail.com>
2020-09-23 21:30:17 +0500

    Fix Content-Security-Policy issue

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
