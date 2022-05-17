0566815c1886f6ebc9386ccfcca8ca20b1719e7b
Safdar Jamal <safdar.dev@gmail.com>
2022-05-18 01:31:56 +0500

    Enable contentSecurityPolicy  middleware

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(helmet({ contentSecurityPolicy: false }));
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
